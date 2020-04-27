c4bea2f153e0c7b3bd123bdc4091bbd3b91fdc88
Willian <wyllmh5@gmail.com>
2020-04-26 20:40:21 -0600

    A lot of changes

---
+++ layouts/error.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="title">
+      {{ message }}
+    </div>
+    <p v-if="statusCode === 404">
+      <nuxt-link to="/">
+        Return to homepage
+      </nuxt-link>
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  head() {
+    return {
+      title: this.message
+    }
+  },
+  computed: {
+    statusCode() {
+      return (this.error && this.error.statusCode) || 500
+    },
+    message() {
+      return this.error.message
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,39 @@
 <template>
   <div>
     <h1>Events</h1>
+    <event-card
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
+
+<script>
+import EventCard from '~/components/EventCard'
+import { mapState } from 'vuex'
+export default {
+  head() {
+    return {
+      title: 'Event Listing'
+    }
+  },
+  components: {
+    EventCard
+  },
+  computed: {
+    ...mapState('events', ['events'])
+  },
+  async fetch({ store, error }) {
+    try {
+      await store.dispatch('events/fetchEvents')
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch events at this time. Please try again.'
+      })
+    }
+  }
+}
+</script>
